7a34854d674b520b47bf0525e818803e824cc2bd
Suraj Prakash <surajprak201@gmail.com>
2026-04-12 17:21:29 +0530

    commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8000
+
+app.get('/', async(req , res)=>{
+    res.send(`
+        <h1>Welcome to my server</h1>
+        <h2>name: Rohit</h2>
+        `)
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+})
